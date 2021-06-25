d671938ec0496e4da8516bca937d8bd42592105c
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-06-25 14:23:11 +0500

    Update AccountController.php

--- app/Http/Controllers/User/AccountController.php
+++ app/Http/Controllers/User/AccountController.php
@@ -9,6 +9,9 @@ use App\Models\User\Venue;
 use App\Models\Location;
 use App\Models\User;
 use App\Models\User\Attached_Account;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -66,6 +69,34 @@ class AccountController extends Controller
      {
 
          return Socialite::driver('facebook')->redirect();
+     }
+
+     public function getFbToken(){
+
+        $fb = new Facebook(array( 
+            'app_id' => env('FACEBOOK_CLIENT_ID'), 
+            'app_secret' => env('FACEBOOK_CLIENT_SECRET'), 
+            'default_graph_version' => 'v11.0', 
+        )); 
+        $helper = $fb->getRedirectLoginHelper();
+
+        try { 
+            if(isset($_SESSION['facebook_access_token'])){ 
+                $accessToken = $_SESSION['facebook_access_token']; 
+            }else{ 
+                  $accessToken = $helper->getAccessToken(); 
+            } 
+        } catch(FacebookResponseException $e) { 
+             echo 'Graph returned an error: ' . $e->getMessage(); 
+              exit; 
+        } catch(FacebookSDKException $e) { 
+            echo 'Facebook SDK returned an error: ' . $e->getMessage(); 
+              exit; 
+        }
+
+        dd($accessToken);
+
+
      }
 
 }
