cb655176fb0c0190aea4b4f02d6406897a109ae4
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-02 18:30:05 +0500

    Update myaccount.blade.php

--- resources/views/users/content/myaccount.blade.php
+++ resources/views/users/content/myaccount.blade.php
@@ -84,7 +84,7 @@
                             </div>
                             <div class="left-0 right-0 bottom-4 absolute text-white px-5 flex flex-col md:flex-row flex-wrap items-center">
                                 <div class="w-full md:w-3/4">
-                                    <a href="#" class="text-xl font-medium">{{$event->event_name}}</a>
+                                    <a href="{{route('socialwall.event',$event)}}" class="text-xl font-medium">{{$event->event_name}}</a>
                                     <p><span class="text-sm m-1">{{$event->hashtag}}</span></p>
                                 </div>
                                 <div class="w-full md:w-1/4 flex justify-end items-center pt-2 md:pt-0">
