332ae766b29a7d4d2d5e6ed6e5bb6760d15da407
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-15 17:38:00 +0500

    Create V_social_wall.php

---
+++ app/Models/V_social_wall.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class V_social_wall extends Model
+{
+    use HasFactory;
+}
