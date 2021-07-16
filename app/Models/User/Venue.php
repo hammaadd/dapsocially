aece17d11e500c685d2e2574dcef3c8203796cfc
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-07-16 16:36:41 +0500

    Update Venue.php

--- app/Models/User/Venue.php
+++ app/Models/User/Venue.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Venue extends Model
 {
     use HasFactory;
+    public function platform(){
+        return $this->hasMany(Venue_Social_Post::class,'venue_id');
+    }
+
+    public function twitter_added(){
+        return $this->platform->where('platform','twitter')->first();
+    }
+
+    public function facebook_added(){
+        return $this->platform->where('platform','facebook')->first();
+    }
+
+    
 }
