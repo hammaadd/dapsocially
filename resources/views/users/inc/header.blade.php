e0e6fa5506b09e1bbab9d2e9e03d7ada97e3a702
Muhammad Adil <muhammadadil79300@gmail.com>
2021-08-04 10:39:10 +0500

    Added Custom StyleSheet

--- resources/views/users/inc/header.blade.php
+++ resources/views/users/inc/header.blade.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - DapSocially</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
     @include('users.inc.fonts')
     <script src="{{asset('js/alpine.min.js')}}" defer></script>
     @yield('headerExtra')
