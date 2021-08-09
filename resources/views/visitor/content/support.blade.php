2b3936d21a49223a7d25924009fc912e844c339d
Muhammad Adil <muhammadadil79300@gmail.com>
2021-08-09 16:55:08 +0500

    Updated Content Pages

--- resources/views/visitor/content/support.blade.php
+++ resources/views/visitor/content/support.blade.php
@@ -1,9 +1,43 @@
 @extends('visitor.layout.visitorLayout')
 @section('title','Contact Support')
 @section('content')
-@include('visitor.inc.homeBanner')
+<nav class="bg-gradient-to-tr from-blue-250 to-blue-550 py-2 relative shadow-md" x-data="{ nav: false}">
+    <div class="max-w-7xl mx-auto flex flex-wrap overflow-hidden items-center justify-between flex-row-reverse md:flex-row px-5">
+        <button class="text-white text-2xl focus:outline-none" @click=" nav = !nav ">
+            <i class="fas fa-bars"></i>
+        </button>
+        <a href="{{route('homepage')}}">
+            <img src="{{asset('assets/logo.png')}}" class="w-44 md:w-56 md:pl-10" alt="DapSocially Logo">
+        </a>
+        <a href="{{route('signup')}}" class="hidden md:block bg-white text-blue-550 md:text-lg uppercase px-6 border-2 border-white rounded-3xl hover:text-white hover:bg-transparent">SIGNUP</a>
+    </div>
+    <div class="w-full sm:w-64 bg-gradient-to-tr from-blue-250 to-blue-550 absolute top-0 left-0 h-screen z-10" x-show="nav" @click.away="nav = !nav" x-transition:enter="transition transform origin-top-left ease-out duration-300" x-transition:enter-start="opacity-0 scale-75" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition transform origin-top-left ease-out duration-300" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-75">
+        <div class="text-right p-5">
+            <i class="fas fa-times text-xl cursor-pointer text-white" @click=" nav = !nav "></i>
+        </div>
+        <ul class="text-center text-white">
+            <li class="py-1"><a href="{{ route('homepage') }}" class="text-xl">Home</a></li>
+            <li class="py-1"><a href="{{route('events')}}" class="text-xl">Events</a></li>
+            <li class="py-1"><a href="{{route('venue')}}" class="text-xl">Venues</a></li>
+            <li class="py-1"><a href="{{route('signin')}}" class="text-xl">Get Started</a></li>
+            <li class="py-2">
+                <a href="{{route('signin')}}" class="bg-white text-blue-550 md:text-lg uppercase px-6 py-1 border-2 border-white rounded-3xl hover:text-white hover:bg-transparent">LOGIN</a>
+            </li>
+        </ul>
+        <hr class="w-4/5 mx-auto my-3 border-gray-400">
+        <ul class="text-gray-300 text-center">
+            <li class="py-1"><a href="tel:3145015100" class="hover:text-white"><i class="fas fa-phone-alt pr-1"></i> (314) 501-5100</a></li>
+            <li class="py-1"><a href="mailto:admin@dapsocially.com" class="hover:text-white"><i class="fas fa-envelope pr-1"></i> admin@dapsocially.com</a></li>
+        </ul>
+        <div class="pt-3 text-center">
+            <a href="#" class="mx-1"><i class="fab fa-facebook-f social-link"></i></a>
+            <a href="#" class="mx-1"><i class="fab fa-twitter social-link"></i></a>
+            <a href="#" class="mx-1"><i class="fab fa-instagram social-link"></i></a>
+        </div>
+    </div>
+</nav>
 <section>
-    <main class="max-w-6xl mx-auto">
+    <main class="max-w-6xl mx-auto min-h-600">
         <h3 class="text-xl font-medium text-center uppercase mt-5">Contact Support</h3>
         {{-- Social Plateforms Section --}}
         <section class="py-5 px-5 lg:py-10">
